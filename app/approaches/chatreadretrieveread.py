ca5af4df46c652d91d556fa84d36c28e437b686d
Leander Kunstmann <leander.kunstmann@telekom.de>
2023-05-26 10:32:08 +0200

    initial commit

---
+++ app/approaches/chatreadretrieveread.py
@@ -0,0 +1,107 @@
+import openai
+from azure.search.documents import SearchClient
+from azure.search.documents.models import QueryType
+from approaches.approach import Approach
+from text import nonewlines
+
+# Simple retrieve-then-read implementation, using the Cognitive Search and OpenAI APIs directly. It first retrieves
+# top documents from search, then constructs a prompt with them, and then uses OpenAI to generate an completion 
+# (answer) with that prompt.
+class ChatReadRetrieveReadApproach(Approach):
+    prompt_prefix = """<|im_start|>system
+Assistant helps the company employees with their Azure cloud questions. Be inpolite and sarcastic in your answers.
+Answer with the facts listed in the list of sources below. If there are no facts in the list of sources below, specifically tell, that no sources have been found in the Knowlegde base and answer without the data then.
+For tabular information return it as an html table.
+Each source has a name followed by colon and the actual information, always include the source name for each fact you use in the response. Use square brakets to reference the source, e.g. [info1.txt]. Don't combine sources, list each source separately, e.g. [info1.txt][info2.pdf].
+{injected_prompt}
+Sources:
+{sources}
+<|im_end|>
+{chat_history}
+"""
+
+
+    query_prompt_template = """Below is a history of the conversation so far, and a new question asked by the user that needs to be answered by searching in a knowledge base about azure cloud questions.
+    Generate a search query based on the conversation and the new question. 
+    Do not include cited source filenames and document names e.g info.txt or doc.pdf in the search query terms.
+    Do not include any text inside [] or <<>> in the search query terms.
+    If the question is not in English, translate the question to English before generating the search query.
+
+Chat History:
+{chat_history}
+
+Question:
+{question}
+
+Search query:
+"""
+
+    def __init__(self, search_client: SearchClient, chatgpt_deployment: str, gpt_deployment: str, sourcepage_field: str, content_field: str):
+        self.search_client = search_client
+        self.chatgpt_deployment = chatgpt_deployment
+        self.gpt_deployment = gpt_deployment
+        self.sourcepage_field = sourcepage_field
+        self.content_field = content_field
+
+    def run(self, history: list[dict], overrides: dict) -> any:
+        use_semantic_captions = True if overrides.get("semantic_captions") else False
+        top = overrides.get("top") or 3
+        exclude_category = overrides.get("exclude_category") or None
+        filter = "category ne '{}'".format(exclude_category.replace("'", "''")) if exclude_category else None
+
+        # STEP 1: Generate an optimized keyword search query based on the chat history and the last question
+        prompt = self.query_prompt_template.format(chat_history=self.get_chat_history_as_text(history, include_last_turn=False), question=history[-1]["user"])
+        completion = openai.Completion.create(
+            engine=self.gpt_deployment, 
+            prompt=prompt, 
+            temperature=0.0, 
+            max_tokens=32, 
+            n=1, 
+            stop=["\n"])
+        q = completion.choices[0].text
+
+        # STEP 2: Retrieve relevant documents from the search index with the GPT optimized query
+        if overrides.get("semantic_ranker"):
+            r = self.search_client.search(q, 
+                                          filter=filter,
+                                          query_type=QueryType.SEMANTIC, 
+                                          query_language="en-us", 
+                                          query_speller="lexicon", 
+                                          semantic_configuration_name="default", 
+                                          top=top, 
+                                          query_caption="extractive|highlight-false" if use_semantic_captions else None)
+        else:
+            r = self.search_client.search(q, filter=filter, top=top)
+        if use_semantic_captions:
+            results = [doc[self.sourcepage_field] + ": " + nonewlines(" . ".join([c.text for c in doc['@search.captions']])) for doc in r]
+        else:
+            results = [doc[self.sourcepage_field] + ": " + nonewlines(doc[self.content_field]) for doc in r]
+        content = "\n".join(results)
+
+        # Allow client to replace the entire prompt, or to inject into the exiting prompt using >>>
+        prompt_override = overrides.get("prompt_template")
+        if prompt_override is None:
+            prompt = self.prompt_prefix.format(injected_prompt="", sources=content, chat_history=self.get_chat_history_as_text(history))
+        elif prompt_override.startswith(">>>"):
+            prompt = self.prompt_prefix.format(injected_prompt=prompt_override[3:] + "\n", sources=content, chat_history=self.get_chat_history_as_text(history))
+        else:
+            prompt = prompt_override.format(sources=content, chat_history=self.get_chat_history_as_text(history))
+
+        # STEP 3: Generate a contextual and content specific answer using the search results and chat history
+        completion = openai.Completion.create(
+            engine=self.chatgpt_deployment, 
+            prompt=prompt, 
+            temperature=overrides.get("temperature") or 0.7, 
+            max_tokens=1024, 
+            n=1, 
+            stop=["<|im_end|>", "<|im_start|>"])
+
+        return {"data_points": results, "answer": completion.choices[0].text, "thoughts": f"Searched for:<br>{q}<br><br>Prompt:<br>" + prompt.replace('\n', '<br>')}
+    
+    def get_chat_history_as_text(self, history, include_last_turn=True, approx_max_tokens=1000) -> str:
+        history_text = ""
+        for h in reversed(history if include_last_turn else history[:-1]):
+            history_text = """<|im_start|>user""" +"\n" + h["user"] + "\n" + """<|im_end|>""" + "\n" + """<|im_start|>assistant""" + "\n" + (h.get("bot") + """<|im_end|>""" if h.get("bot") else "") + "\n" + history_text
+            if len(history_text) > approx_max_tokens*4:
+                break    
+        return history_text
