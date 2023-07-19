23810b30702b8028793b5b028438421af2cd4e11
Leander Kunstmann <leander.kunstmann@telekom.de>
2023-07-19 13:09:23 +0200

    fixed docs (from content)

--- app/app.py
+++ app/app.py
@@ -12,7 +12,7 @@ from azure.storage.blob import BlobServiceClient
 
 # Replace these with your own values, either in environment variables or directly here
 AZURE_STORAGE_ACCOUNT = os.environ.get("AZURE_STORAGE_ACCOUNT") or "mystorageaccount"
-AZURE_STORAGE_CONTAINER = os.environ.get("AZURE_STORAGE_CONTAINER") or "content"
+AZURE_STORAGE_CONTAINER_DOCS = os.environ.get("AZURE_STORAGE_CONTAINER_DOCS") or "docs"
 AZURE_SEARCH_SERVICE = os.environ.get("AZURE_SEARCH_SERVICE") or "gptkb"
 AZURE_SEARCH_INDEX = os.environ.get("AZURE_SEARCH_INDEX") or "gptkbindex"
 AZURE_OPENAI_SERVICE = os.environ.get("AZURE_OPENAI_SERVICE") or "myopenai"
@@ -50,7 +50,7 @@ search_client = SearchClient(
 blob_client = BlobServiceClient(
     account_url=f"https://{AZURE_STORAGE_ACCOUNT}.blob.core.windows.net", 
     credential=azure_credential)
-blob_container = blob_client.get_container_client(AZURE_STORAGE_CONTAINER)
+blob_container = blob_client.get_container_client(AZURE_STORAGE_CONTAINER_DOCS)
 
 # Various approaches to integrate GPT and external knowledge, most applications will use a single one of these patterns
 # or some derivative, here we include several for exploration purposes
