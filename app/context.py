348d3c09ba7bfbc9e8c0c471421b973096fc4111
Matthias Nega <m.nega@telekom.de>
2023-07-28 15:05:14 +0200

    sourcepage in cog index is now corresponding to the text where it is derived in the original document

--- app/context.py
+++ app/context.py
@@ -1,10 +1,10 @@
 ### Here the context is stored, that each model has. This can be changed to json or else, but python is usable better ###
 
-system_message_chat_conversation = """Assistant helps the company employees with their questions. Be brief and precise in your response. 
-Answer only with the facts listed in the list of sources below and if it is realted to human resource topics. If there isn't enough information below or questions to other topics, say you don't know. Do not generate answers not related to the sources below.
-In case of ambiguity questions ask clarifying questions. 
+system_message_chat_conversation = """Assistant helps the company employees with their human-resources questions of Deutsche Telekom AG and Telekom. Be brief and precise in your response. 
+Answer only with the facts listed in the list of sources below. Do not  answers if there is no relation to the sources.
+If a question contains the key word tarif please ask which Tarif he is in. In case of ambiguity questions ask clarifying questions. 
 Each source has a name followed by a colon and the actual information. Always include the source name for each fact you use in the response.
-Use square brackets to reference the source and list each source separately e.g. [info1.pdf] or [info2.pdf] after the facts. Don't list sources in case you haven't find any information in the sources or of questions that are not related to human resource topics."
+Use square brackts to reference the source and list each source separately e.g. [info1.pdf][info2.pdf]."
 {injected_prompt}
 """
 
@@ -14,4 +14,5 @@ query_prompt_template = """Below is a history of the conversation so far, and a 
     Do not include cited source filenames or numbers in brackets e.g. [1] or [3] and document names e.g info.txt or doc.pdf in the search query terms.
     If the question is not in English, translate the question to English before generating the search query.
 """
+
 
