b0da63233be6d4181df4a593f74be31fbe05198e
Leander Kunstmann <leander.kunstmann@telekom.de>
2023-09-12 14:22:25 +0200

    current status of feedback and rewrite of debug

--- app/backend/app.py
+++ app/backend/app.py
@@ -1,5 +1,4 @@
 import io
-import logging
 import mimetypes
 import os
 import time
@@ -14,6 +13,7 @@ from azure.monitor.opentelemetry import configure_azure_monitor
 from azure.search.documents.aio import SearchClient
 from azure.storage.blob.aio import BlobServiceClient
 from opentelemetry.instrumentation.aiohttp_client import AioHttpClientInstrumentor
+from opentelemetry.instrumentation.logging import LoggingInstrumentor
 from opentelemetry.instrumentation.asgi import OpenTelemetryMiddleware
 from quart import (
     Blueprint,
@@ -26,10 +26,28 @@ from quart import (
 )
 
 from quart_cors import cors
-from quart_schema import QuartSchema, Info, validate_request, validate_response, document_response
+from quart_schema import QuartSchema, Info, document_request, document_response
 
 from approaches.chatreadretrieveread import ChatReadRetrieveReadApproach
 from core.modelhelper import cgsIndexColumnFacetDist
+
+import logging
+# Define the EVAL level
+EVAL = 35
+logging.addLevelName(EVAL, "EVAL")
+
+def eval(self, message, *args, **kws):
+    if self.isEnabledFor(EVAL):
+        # Yes, logger takes its '*args' as 'args'.
+        self._log(EVAL, message, args, **kws)
+
+# Add the method to logging.Logger
+logging.Logger.eval = eval
+
+# Now you can use EVAL as an argument to logging functions:
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.WARNING)  # Set threshold
+
 
 CONFIG_OPENAI_TOKEN = "openai_token"
 CONFIG_CREDENTIAL = "azure_credential"
@@ -43,7 +61,6 @@ bp = Blueprint("routes", __name__, static_folder='static')
 if platform.system() == 'Darwin':
     print('cors disabled')
     bp = cors(bp, allow_origin="*")
-
 
 @dataclass
 class History:
@@ -59,7 +76,7 @@ class Overrides:
     temperature: float
 
 @dataclass
-class RequestData:
+class ChatRequestData:
     history: List[History]
     approach: str = "rrr"
     overrides: Optional[Overrides] = None
@@ -70,15 +87,77 @@ class DataPoint:
     page: int
 
 @dataclass
-class ResponseData:
+class ChatResponseData:
     answer: str
     thoughts: str
-    data_points: List[DataPoint]
+    data_points: List[DataPoint] = None
+
+@dataclass
+class ErrorChatResponseData:
+    answer: str
+    thoughts: str
 
 @dataclass
 class CatResponse:
     categories: List[str]
 
+@dataclass
+class ErrorResponse:
+    error: str
+
+@dataclass
+class FeedbackRequestData:
+    history: List[History]
+    opinion: bool
+
+@dataclass
+class FeedbackResponseData:
+    response: str
+
+
+@bp.route("/category", methods=["GET"])
+@document_response(CatResponse, 200)
+@document_response(ErrorResponse, 400)
+async def category():
+    try:
+        search_client = current_app.config[CONFIG_SEARCH_CLIENT]
+        search_res = await cgsIndexColumnFacetDist(search_client, "category")
+        values = [item['value'] for item in search_res]
+        res = {"categories": values}
+        return jsonify(res)
+    except Exception as e:
+        res = {"error": e.args[0]}
+        return jsonify({"error": str(e)}), 500
+    
+
+
+@bp.route("/chat", methods=["POST"])
+@document_request(ChatRequestData)
+@document_response(ChatResponseData, 200)
+@document_response(ErrorChatResponseData, 400)
+@document_response(ErrorChatResponseData, 429)
+@document_response(ErrorChatResponseData, 500)
+async def chat():
+    if not request.is_json:
+        return jsonify({"error": "request must be json"}), 415
+    request_json = await request.get_json()
+    approach = request_json["approach"]
+    try:
+        impl = current_app.config[CONFIG_CHAT_APPROACHES].get(approach)
+        if not impl:
+            return jsonify({"error": "unknown approach"}), 400
+        # Workaround for: https://github.com/openai/openai-python/issues/371
+        async with aiohttp.ClientSession() as s:
+            openai.aiosession.set(s)
+            r = await impl.run(request_json["history"], request_json.get("overrides") or {})
+        if (r["thoughts"] == "error"):
+            return (jsonify(r)), 500
+        elif (r["thoughts"] == "ratelimit"):
+            return (jsonify(r)), 429
+        return jsonify(r)
+    except Exception as e:
+        logger.exception("Exception in /chat")
+        return jsonify({"error": str(e)}), 500
 
 # Serve content files from blob storage from within the app to keep the example self-contained.
 # *** NOTE *** this assumes that the content files are public, or at least that all users of the app
@@ -97,36 +176,19 @@ async def content(path):
     blob_file.seek(0)
     return await send_file(blob_file, mimetype=mime_type, as_attachment=False, attachment_filename=path)
 
-
-@bp.route("/category", methods=["GET"])
-@validate_response(CatResponse)
-async def category():
-    search_client = current_app.config[CONFIG_SEARCH_CLIENT]
-    search_res = await cgsIndexColumnFacetDist(search_client, "category")
-    values = [item['value'] for item in search_res]
-    res = {"categories": values}
-    return jsonify(res)
-
-@bp.route("/chat", methods=["POST"])
-@validate_request(RequestData)
-@validate_response(ResponseData)
-async def chat():
-    if not request.is_json:
-        return jsonify({"error": "request must be json"}), 415
-    request_json = await request.get_json()
-    approach = request_json["approach"]
-    try:
-        impl = current_app.config[CONFIG_CHAT_APPROACHES].get(approach)
-        if not impl:
-            return jsonify({"error": "unknown approach"}), 400
-        # Workaround for: https://github.com/openai/openai-python/issues/371
-        async with aiohttp.ClientSession() as s:
-            openai.aiosession.set(s)
-            r = await impl.run(request_json["history"], request_json.get("overrides") or {})
-        return jsonify(r)
+@bp.route("/feedback", methods=["POST"])
+@document_request(FeedbackRequestData)
+@document_response(FeedbackResponseData)
+@document_response(ErrorChatResponseData, 400)
+async def feedback():
+    try: 
+        request_json = await request.get_json()
+        his = request_json
+        logger.eval(str(his))
+        return jsonify({"response": "feedback has been forwarded"})
     except Exception as e:
-        logging.exception("Exception in /chat")
         return jsonify({"error": str(e)}), 500
+
 
 @bp.before_request
 async def ensure_openai_token():
@@ -225,9 +287,11 @@ def create_app():
     if os.getenv("APPLICATIONINSIGHTS_CONNECTION_STRING"):
         configure_azure_monitor()
         AioHttpClientInstrumentor().instrument()
+        LoggingInstrumentor().instrument()
+    
     app = Quart(__name__)
     app.register_blueprint(bp)
     app.asgi_app = OpenTelemetryMiddleware(app.asgi_app)
-    QuartSchema(app, info=Info(title="Telekom LLM & CompanyData API", version="0.7"))
+    QuartSchema(app, info=Info(title="Telekom LLM & CompanyData API",version="1.0"))
 
     return app
--- app/backend/approaches/chatreadretrieveread.py
+++ app/backend/approaches/chatreadretrieveread.py
@@ -2,6 +2,7 @@ from typing import Any
 import time
 import json 
 import os
+import logging
 
 import openai
 from azure.search.documents.aio import SearchClient
@@ -43,25 +44,35 @@ class ChatReadRetrieveReadApproach(ChatApproach):
         #initialize overrides
         has_text = overrides.get("retrieval_mode") in ["text", "hybrid", None]
         has_vector = overrides.get("retrieval_mode") in ["vectors", "hybrid", None]
+
         use_semantic_captions = True if overrides.get("semantic_captions") and has_text else False
         top = overrides.get("top") or 3
         ''' Building the category filter in dependence of the given path >ToDo: must be implemented'''
+
         exclude_category = overrides.get("exclude_category") or None
         category_filter = "category ne '{}'".format(exclude_category.replace("'", "''")) if exclude_category else None
         ''' Building the language filter accordig to the language in the user prompt and in dependence of multilingual_search setting (True|False)'''
+
         lang_facets = json.loads(os.getenv('FACETS_RESULTS').replace("'", '"'))
+
         print(lang_facets)
+
         default_lang = getLang(lang_facets[0]['value']) if lang_facets else {'iso': 'de','name': 'German'}
+
         print('The default lang is: ' + str(default_lang)) 
+
         supported_languages = []
         for rec in lang_facets:
             supported_languages.append(getLang(rec['value']))
+
         print('Supported Languages : ' + str(supported_languages))
+
         user_prompt_lang = detectLang(history[-1]["user"])
         user_prompt_lang_name = getLang(user_prompt_lang)
         lang_name = next((rec.get('name') for rec in supported_languages if user_prompt_lang in rec['iso']), default_lang['name'])
         lang = next((rec.get('iso') for rec in supported_languages if user_prompt_lang in rec['iso']), default_lang['iso'])
         noidea_de_text = 'Tut mir leid, ich weiss das nicht'
+
         system_message_noidea = noidea_de_text if user_prompt_lang == 'de' else translateText(noidea_de_text, user_prompt_lang_name['name'],self.chatgpt_deployment) if user_prompt_lang != 'de' else "Sorry, I don't know"
         ''' Multilngual search is the default. It is prior because it handles english text and german language in screen shots better '''
         multilingual_search = overrides.get("multilingual_search") or True
@@ -110,59 +121,38 @@ class ChatReadRetrieveReadApproach(ChatApproach):
             # start timing request
             start_keyword = time.perf_counter()
 
-            try: 
                 # completion request to openAI
 
-                chat_completion = await openai.ChatCompletion.acreate(
-                    deployment_id=self.chatgpt_deployment,
-                    model=self.chatgpt_model,
-                    messages=messages, 
-                    temperature=0.0, 
-                    max_tokens=self.max_tokens_query, 
-                    n=1)
-                
-                query_text = chat_completion.choices[0].message.content
+            chat_completion = await openai.ChatCompletion.acreate(
+                deployment_id=self.chatgpt_deployment,
+                model=self.chatgpt_model,
+                messages=messages, 
+                temperature=0.0, 
+                max_tokens=self.max_tokens_query, 
+                n=1)
+            
+            query_text = chat_completion.choices[0].message.content
 
-                if query_text.strip() == "0":
-                    query_text = history[-1]["user"] # Use the last user input if we failed to generate a better query
+            if query_text.strip() == "0":
+                query_text = history[-1]["user"] # Use the last user input if we failed to generate a better query
 
-                addTokenCount(usedTokens, chat_completion)
-
-                # debug
-                print(f"\nUmwandlung der Frage in Keywords: {query_text}\n")
-
-            except Exception as e:
-                raise Exception({
-                    'exception': e.args[0], 
-                    'info': {
-                        'tokens': num_tokens_from_messages(messages, self.chatgpt_model)
-                    }
-                    })
+            addTokenCount(usedTokens, chat_completion)
 
             # save time for completion request for keyword optimization and add token count to request token object
             keyword_request_time = round(time.perf_counter() - start_keyword, r_dec)
 
             # STEP 2: Retrieve relevant documents from the search index with the GPT optimized query
-
             # If retrieval mode includes vectors, compute an embedding for the query
             if has_vector:
                 
                 start_embedding = time.perf_counter()
 
-                try:
-                    query_vector_embedding = await openai.Embedding.acreate(
-                        engine=self.embedding_deployment, 
-                        input=query_text)
-                    query_vector = query_vector_embedding.data[0].embedding
+                query_vector_embedding = await openai.Embedding.acreate(
+                    engine=self.embedding_deployment, 
+                    input=query_text)
+                query_vector = query_vector_embedding.data[0].embedding
 
-                    addTokenCount(usedTokens, query_vector_embedding)
-
-                except Exception as e: 
-                    raise Exception({
-                        'exception': e.args[0], 
-                        'info': {
-                            'tokens': num_tokens_from_messages(query_text, 'text-embedding-ada-002')
-                            }})
+                addTokenCount(usedTokens, query_vector_embedding)
 
                 embedding_request_time = round(time.perf_counter() - start_embedding, r_dec)
 
@@ -179,46 +169,28 @@ class ChatReadRetrieveReadApproach(ChatApproach):
             start_cog_search = time.perf_counter()
 
             if overrides.get("semantic_ranker") and has_text:
-                try:
-                    r = await self.search_client.search(
-                        query_text, 
-                        filter=filter,
-                        query_type=QueryType.SEMANTIC, 
-                        query_language= cgs_query_languages.get(lang,'en-us'), 
-                        query_speller="lexicon", 
-                        semantic_configuration_name="default", 
-                        top=top, 
-                        query_caption="extractive|highlight-false" if use_semantic_captions else None,
-                        vector=query_vector,
-                        top_k=50 if query_vector else None,
-                        vector_fields="embedding" if query_vector else None)
-                    
-                except Exception as e: 
-                    raise Exception({
-                        'exception': e.args[0],
-                        'info': {
-                            'top': top, 
-                            'query_text': query_text
-                            }})
+                r = await self.search_client.search(
+                    query_text, 
+                    filter=filter,
+                    query_type=QueryType.SEMANTIC, 
+                    query_language= cgs_query_languages.get(lang,'en-us'), 
+                    query_speller="lexicon", 
+                    semantic_configuration_name="default", 
+                    top=top, 
+                    query_caption="extractive|highlight-false" if use_semantic_captions else None,
+                    vector=query_vector,
+                    top_k=50 if query_vector else None,
+                    vector_fields="embedding" if query_vector else None)
                 
             else:
-                try:
-                    r = await self.search_client.search(
-                        query_text, 
-                        filter=filter, 
-                        query_language= cgs_query_languages.get(lang,'en-us'), 
-                        top=top, 
-                        vector=query_vector,
-                        top_k=50 if query_vector else None, 
-                        vector_fields="embedding" if query_vector else None)
-                except Exception as e: 
-                    raise Exception({
-                        'exception': e.args[0],
-                        'info': {
-                            'top': top, 
-                            'query_text': query_text
-                            }})
-                
+                r = await self.search_client.search(
+                    query_text, 
+                    filter=filter, 
+                    query_language= cgs_query_languages.get(lang,'en-us'), 
+                    top=top, 
+                    vector=query_vector,
+                    top_k=50 if query_vector else None, 
+                    vector_fields="embedding" if query_vector else None)
             
             cog_search_request_time = round(time.perf_counter() - start_cog_search, r_dec)
             
@@ -227,9 +199,6 @@ class ChatReadRetrieveReadApproach(ChatApproach):
             else:
                 results = [doc[self.sourcepage_field] + ": " + nonewlines(doc[self.content_field]) async for doc in r]
             content = "\n".join(results)
-
-            # debug
-            print(f"\nContent aus der Suche: \n-----\n{content}\n-----\n")
 
 
             # Allow client to replace the entire prompt, or to inject into the exiting prompt using >>>
@@ -242,42 +211,36 @@ class ChatReadRetrieveReadApproach(ChatApproach):
             # STEP 3: Generate a contextual and content specific answer using the search results and chat history
             main_llm_req_start = time.perf_counter()
 
-            try:
+            messages = self.get_messages_from_history(
+                        system_message,
+                        self.chatgpt_model,
+                        history,
+                        history[-1]["user"]+ "\n\nSources:\n" + content, # Model does not handle lengthy system messages well. Moving sources to latest user conversation to solve follow up questions prompt.
+                        max_tokens=self.chatgpt_token_limit)
 
-                messages = self.get_messages_from_history(
-                            system_message,
-                            self.chatgpt_model,
-                            history,
-                            history[-1]["user"]+ "\n\nSources:\n" + content, # Model does not handle lengthy system messages well. Moving sources to latest user conversation to solve follow up questions prompt.
-                            max_tokens=self.chatgpt_token_limit)
-
-                chat_completion = await openai.ChatCompletion.acreate(
-                    deployment_id=self.chatgpt_deployment,
-                    model=self.chatgpt_model,
-                    messages=messages, 
-                    temperature=overrides.get("temperature") or 0.7, 
-                    max_tokens=self.max_tokens_answer, 
-                    n=1)
-                
-                addTokenCount(usedTokens, chat_completion)
-
-
-            except Exception as e: 
-                    raise Exception({
-                        'exception': e.args[0],
-                        'info': {
-                            'prompt': num_tokens_from_messages(messages, self.chatgpt_model), 
-                            'query_text': query_text
-                            }})
+            chat_completion = await openai.ChatCompletion.acreate(
+                deployment_id=self.chatgpt_deployment,
+                model=self.chatgpt_model,
+                messages=messages, 
+                temperature=overrides.get("temperature") or 0.7, 
+                max_tokens=self.max_tokens_answer, 
+                n=1)
             
+            addTokenCount(usedTokens, chat_completion)
+
             main_llm_req_time = round(time.perf_counter() - main_llm_req_start, r_dec)
 
             chat_time = round(time.perf_counter() - start_chat, r_dec)
 
-        # if one of the request fails, this new object will be appended to the logs    
+        # handle all errors except rate limit the same, as no difference is needed here
         except Exception as e:
             exc = True
             errorMessage = e.args[0]
+            error_res = { "answer": errorMessage, "thoughts": "error"}
+        except openai.error.RateLimitError as e:
+            exc = True
+            errorMessage = e
+            error_res = { "answer": errorMessage, "thoughts": "ratelimit"}
 
         # define logs for applicationinsights
         log_values = {
@@ -294,31 +257,15 @@ class ChatReadRetrieveReadApproach(ChatApproach):
         for key, value in usedTokens.items():
             log_values[key] = value
 
-        if exc: 
+        if exc:
             log_values["status"] = "error"
             log_values["errorMessage"] = errorMessage
+            logging.error(log_values)
+            return error_res
 
-        # add to properties for usage in logger
-        properties = {"custom_dimensions": log_values}
-
-        #TODO: implement .env (not pushed to gitlab) logic for the logs 
-        # logger = logging.getLogger(__name__)
-        # logger.addHandler(AzureLogHandler('put in connectionstring here')
-        # logger.warning('chat_request', extra=properties)
-
-        print(log_values)
-        
-        # get list of citatiuons in text and provide it as text seperated by a new line
-        
-        if exc:
-            return -1
-        
-        # Extract sources with specific information to be used by potential frontend for single pages and full document usage
+        logging.info(log_values)
 
         chat_content = chat_completion.choices[0].message.content
-
-        # debug
-        print(f"\nAntwort von ChatGPT: \n-----\n{chat_content}\n-----\n")
 
         msg_to_display = '\n\n'.join([str(message) for message in messages])
 
