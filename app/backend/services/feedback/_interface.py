462da275d93888294de7ca6b96667e0608f18a34
lukas.klein <lukas.klein@telekom.de>
2025-05-06 12:05:43 +0200

    feat(feedback): added categories to feedback

--- app/backend/services/feedback/_interface.py
+++ app/backend/services/feedback/_interface.py
@@ -1,6 +1,7 @@
 from abc import ABC
 from abc import abstractmethod
 from dataclasses import dataclass
+from dataclasses import field
 from typing import List
 from typing import Union
 
@@ -18,6 +19,7 @@ class Feedback:
     history: List[ChatMessage]
     rating: int
     message: str
+    categories: List[str] = field(default_factory=list)
 
     def __post_init__(self):
         """__post_init__ is responsible for the deep conversion of the dataclass fields."""
@@ -31,12 +33,13 @@ class Feedback:
             ]
             # fmt: on
 
-    def to_dict(self) -> dict[str, Union[str, int, list[ChatMessageDict]]]:
+    def to_dict(self) -> dict[str, Union[str, int, list[ChatMessageDict], list[str]]]:
         """to_dict converts the dataclass to a dictionary."""
         return {
             "message": self.message,
             "rating": self.rating,
             "history": [item.to_dict() for item in self.history],
+            "categories": self.categories,
         }
 
 
